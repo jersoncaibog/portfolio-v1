cb4d4042c03da81e4dda3220ba77c65d486ca11f
jersoncaibog <jersoncaibog0423@gmail.com>
2025-07-29 21:22:40 +0800

    updated projects

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -203,7 +203,7 @@ const blog = {
 
 const work = {
   path: "/work",
-  label: "Work",
+  label: "Projects",
   title: `Featured Projects`,
   description: `Design and dev projects by ${person.name}`,
   // Create new project pages by adding a new .mdx file to app/blog/posts
